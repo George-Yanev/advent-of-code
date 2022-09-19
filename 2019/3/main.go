c87f2f1eee67ce2038cddc74428eab4b5ba26468
George Yanev <gyanev@coursera.org>
2022-09-19 10:18:23 +0300

    First working version + the input

--- 2019/3/main.go
+++ 2019/3/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
+	"math"
+	"os"
 	"strconv"
 	"strings"
 )
-
-const snake1Input string = "R75,D30,R83,U83,L12,D49,R71,U7,L72"
-const snake2Input string = "U62,R66,U55,R34,D71,R55,D58,R83"
 
 // 74, 0
 // 74, -29
@@ -21,11 +22,9 @@ func (s snakePoint) String() string {
 	return fmt.Sprintf("snakePoint{x:%v, y:%v}", s.x, s.y)
 }
 
-var snake []*snakePoint
-
-func newSnakePath(snakeString string) []*snakePoint {
+func newSnakePath(snakeString string) []snakePoint {
 	snakeSlice := strings.Split(snakeString, ",")
-	snake = append(snake, &snakePoint{0, 0})
+	snake := []snakePoint{{0, 0}}
 
 	for i := 0; i < len(snakeSlice); i++ {
 		steps, err := strconv.Atoi(snakeSlice[i][1:])
@@ -36,41 +35,82 @@ func newSnakePath(snakeString string) []*snakePoint {
 		p := snake[len(snake)-1]
 		switch d := snakeSlice[i][0]; string(d) {
 		case "L":
-			snake = append(snake, &snakePoint{p.x - steps, p.y})
+			for i := 1; i <= steps; i++ {
+				snake = append(snake, snakePoint{p.x - i, p.y})
+			}
 		case "R":
-			snake = append(snake, &snakePoint{p.x + steps, p.y})
+			for i := 1; i <= steps; i++ {
+				snake = append(snake, snakePoint{p.x + i, p.y})
+			}
 		case "D":
-			snake = append(snake, &snakePoint{p.x, p.y - steps})
+			for i := 1; i <= steps; i++ {
+				snake = append(snake, snakePoint{p.x, p.y - i})
+			}
 		case "U":
-			snake = append(snake, &snakePoint{p.x, p.y + steps})
+			for i := 1; i <= steps; i++ {
+				snake = append(snake, snakePoint{p.x, p.y + i})
+			}
 		}
 	}
 	return snake
 }
 
-func intersection(a, b []*snakePoint) ([]*snakePoint, error) {
+func intersection(a, b []snakePoint) ([]snakePoint, error) {
 
 	// uses empty struct (0 bytes) for map values.
-	m := make(map[*snakePoint]struct{}, len(b))
+	m := make(map[snakePoint]struct{}, len(b))
 
 	// cached
 	for _, v := range b {
 		m[v] = struct{}{}
 	}
 
-	var s []*snakePoint
+	var s []snakePoint
 	for _, v := range a {
 		if _, ok := m[v]; ok {
 			s = append(s, v)
 		}
 	}
 
-	return s, nil
+	return s[1:], nil
+}
+
+func manhattanDistance(s []snakePoint) (int, error) {
+	md := math.MaxInt
+	for _, p := range s {
+		if cd := int(math.Abs(float64(p.x)) + math.Abs(float64(p.y))); cd < md {
+			md = cd
+		}
+	}
+	if md == math.MaxInt {
+		return md, errors.New("No Manhattan Distance found")
+	}
+	return md, nil
+
 }
 
 func main() {
-	snake1 := newSnakePath(snake1Input)
-	snake2 := newSnakePath(snake2Input)
-	fmt.Printf("Snake1 is: %v\n", snake1)
-	fmt.Printf("Snake2 is: %v\n", snake2)
+	f, err := os.Open("input")
+	if err != nil {
+		fmt.Errorf("err: %v", err)
+	}
+	fileScanner := bufio.NewScanner(f)
+	fileScanner.Split(bufio.ScanLines)
+	var snakes []string
+	for fileScanner.Scan() {
+		snakes = append(snakes, fileScanner.Text())
+	}
+
+	snake1 := newSnakePath(snakes[0])
+	snake2 := newSnakePath(snakes[1])
+	intersect, err := intersection(snake1, snake2)
+	if err != nil {
+		fmt.Errorf("Error while creating the intersection - %v", err)
+	}
+
+	d, err := manhattanDistance(intersect)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("Manhattan Distance: %v\n", d)
 }
