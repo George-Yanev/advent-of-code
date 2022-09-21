c479ec6f3390c5a2b2eb00ca5ef98371ee8c71da
George Yanev <gyanev@coursera.org>
2022-09-21 21:42:08 +0300

    First part of day 4 completed

--- 2019/4/main.go
+++ 2019/4/main.go
@@ -6,21 +6,30 @@ import (
 )
 
 const start int = 109165
-// const end int = 576723
-const end int = 111222
+const end int = 576723
+
+// const end int = 123789
 
 func nextIncreasing(n int) int {
+	//fmt.Println("Next number to increase: ", n)
 	var closestIncreasing string
 	s := strconv.Itoa(n)
+	var m = map[bool]int{}
 	for i, d := range s {
 		if i == 0 {
 			closestIncreasing += string(d)
 			continue
 		}
 
-		p := int(closestIncreasing[i-1])
-		if int(d) < p {
+		if i, ok := m[true]; ok {
+			closestIncreasing += string(s[i])
+			continue
+		}
+
+		p := closestIncreasing[i-1]
+		if int(d) < int(p) {
 			closestIncreasing += string(p)
+			m[true] = i - 1
 		} else {
 			closestIncreasing += string(d)
 		}
@@ -32,6 +41,19 @@ func nextIncreasing(n int) int {
 	return c
 }
 
+func checkDoubles(n int) bool {
+	s := strconv.Itoa(n)
+	for i, d := range s {
+		if i == 0 {
+			continue
+		}
+		if int(s[i-1]) == int(d) {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
 	var passwords []int
 	loopBreaker := true
@@ -39,7 +61,8 @@ func main() {
 	for loopBreaker {
 		for i := currentStart; i <= end; i++ {
 			n := nextIncreasing(i)
-			f n >= end {
+			//fmt.Println("Current number: ", n)
+			if n >= end {
 				loopBreaker = false
 			}
 
@@ -47,9 +70,11 @@ func main() {
 				currentStart = n
 				break
 			}
-			passwords = append(passwords, n)
+			if checkDoubles(n) {
+				passwords = append(passwords, n)
+			}
 		}
 	}
 	fmt.Printf("Number of possible passwords: %d\n", len(passwords))
-	fmt.Printf("List the passwords: %v", passwords)
+	//fmt.Printf("List the passwords: %v", passwords)
 }
