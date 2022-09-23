97d8fdf44025633ac81850a7912411228650a659
George Yanev <gyanev@coursera.org>
2022-09-23 18:58:00 +0300

    Day 2 - second task
    - First task will not work but it will be trivial to fixed the noun and
      verb as variables

--- 2019/2/main.go
+++ 2019/2/main.go
@@ -25,7 +25,13 @@ func ReadInts(r io.Reader) ([]int, error) {
 	return result, scanner.Err()
 }
 
-func ReadFile(r io.Reader) []int {
+func ReadFile(f string) []int {
+	r, err := os.Open(f)
+	if err != nil {
+		fmt.Println("error reading the file", err)
+	}
+	defer r.Close()
+
 	reader := csv.NewReader(r)
 	var input []string
 	var final []int
@@ -47,38 +53,53 @@ func ReadFile(r io.Reader) []int {
 }
 
 func main() {
-	f, err := os.Open("input")
-	if err != nil {
-		fmt.Println("error reading the file", err)
-	}
-	defer f.Close()
-	input := ReadFile(f)
-	fmt.Println(len(input))
-
-	i := 0
-	for i < len(input) {
-		var opcode, input1, input2, output int
-		if len(input) > i+3 {
-			opcode = input[i]
-			input1 = input[i+1]
-			input2 = input[i+2]
-			output = input[i+3]
-			i += 4
-		} else {
-			fmt.Println("end reached")
-			fmt.Println(input[i:])
-			opcode = input[i]
-			i = len(input)
-		}
-		switch {
-		case opcode == 1:
-			input[output] = input[input1] + input[input2]
-		case opcode == 2:
-			input[output] = input[input1] * input[input2]
-		case opcode == 99:
-			fmt.Println("reached opcode 99...exit")
-			fmt.Println(input)
+	var noun, verb int
+	for {
+		fmt.Printf("Testin with noun: %d and verb: %d\n", noun, verb)
+		input := ReadFile("input")
+		//fmt.Println(len(input))
+		input[1] = noun
+		input[2] = verb
+		if noun+verb > 198 {
+			fmt.Printf("noun: %d and verb: %d reached their limit\n", noun, verb)
 			return
+		}
+		i := 0
+		for i < len(input) {
+			var opcode, input1, input2, output int
+			if len(input) > i+3 {
+				opcode = input[i]
+				input1 = input[i+1]
+				input2 = input[i+2]
+				output = input[i+3]
+				i += 4
+			} else {
+				//fmt.Println("end reached")
+				//fmt.Println(input[i:])
+				opcode = input[i]
+				i = len(input)
+			}
+			//fmt.Println("i is: ", i)
+			switch {
+			case opcode == 1:
+				input[output] = input[input1] + input[input2]
+			case opcode == 2:
+				input[output] = input[input1] * input[input2]
+			case opcode == 99:
+				//fmt.Println("reached opcode 99...exit")
+				fmt.Println("result is: ", input[0])
+				if input[0] == 19690720 {
+					fmt.Printf("Found...noun is %d, verb is %d\n", noun, verb)
+					fmt.Printf("answer is: %d\n", 100*noun+verb)
+					return
+				}
+			}
+		}
+		if verb < 99 {
+			verb++
+		} else if verb == 99 {
+			noun++
+			verb = 0
 		}
 	}
 }
